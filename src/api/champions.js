750ee5ef67f926ed6932e9fdccf74d4b4e037d1c
Guille <guillermobernallou@gmail.com>
2024-03-27 12:52:24 +0100

    Remove champions API endpoint

--- src/api/champions.js
+++
@@ -1,8 +0,0 @@
-// File: pages/api/champions.js
-import { DDragon } from '@fightmegg/riot-api';
-
-export default async function handler(req, res) {
-    const ddragon = new DDragon();
-    const champsData = await ddragon.champion.all();
-    res.status(200).json(champsData.data);
-}
